2018688cb169c541b35575e02d663d990838bb05
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-10-03 11:51:28 -0700

    Connect MongoDB using Mongoose

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config()
 
 const PORT = process.env.PORT || 3000;
+
+const dbURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@url-shortener.jnq1j.mongodb.net/urls?retryWrites=true&w=majority`;
+
+// Connect to MongoDB Atlas
+mongoose
+	.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	// (node:18781) DeprecationWarning: current Server Discovery and Monitoring engine is deprecated, and will be removed in a future version. To use the new Server Discover and Monitoring engine, pass option { useUnifiedTopology: true } to the MongoClient constructor.
+	.then(console.log("MongoDB Connected."))
+	.catch((error) => console.log(error));
 
 const app = express();
 
