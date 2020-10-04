b0442f909be27dfa81ce68a8f5bd80b28e0c53c7
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2020-10-03 21:35:42 -0700

    Change to MongoDB connection and driver

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const morgan = require("morgan")
-const mongoose = require("mongoose");
+const mongodb = require("mongodb");
 const { json } = require("body-parser");
+const { mongo } = require("mongoose");
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3000;
 
-const dbURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@url-shortener.jnq1j.mongodb.net/urls?retryWrites=true&w=majority`;
+const dbURI = process.env.DB_URI;
 
 // Connect to MongoDB Atlas
-mongoose
-	.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-	// (node:18781) DeprecationWarning: current Server Discovery and Monitoring engine is deprecated, and will be removed in a future version. To use the new Server Discover and Monitoring engine, pass option { useUnifiedTopology: true } to the MongoClient constructor.
-	.then(console.log("MongoDB Connected."))
-	.catch((error) => console.log(error));
+let collection;
+const client = new mongodb.MongoClient(dbURI, {useUnifiedTopology: true})
+client.connect((error, connection) => {
+	if (error) {
+		throw(error)
+	}
+	else {
+		console.log("Successfully connected to MongoDB")
+		collection = connection.db("urls").collection("urls")
+	}
+})
 
 const app = express();
 
